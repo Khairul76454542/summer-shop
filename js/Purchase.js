351533e39f4f2c0535f32f89edc465c177ebd560
khairul76454542 <khairul76454542@gmail.com>
2023-08-22 23:55:34 +0600

    first commit

---
+++ js/Purchase.js
@@ -0,0 +1,76 @@
+function card1() {
+  const card1Price = document.getElementById("card-1").innerText;
+  const totalPriceElement = document.getElementById("total-price");
+  const totalPrice = parseFloat(totalPriceElement.innerText);
+  const total = parseFloat(card1Price) + totalPrice;
+  totalPriceElement.innerText = total;
+  document.getElementById("final").innerText = total;
+
+  document.getElementById("final").innerText = total;
+  const cardTitle = document.getElementById("title-1").innerText;
+  const list = document.createElement("li");
+  list.innerHTML = cardTitle;
+  document.getElementById("list").appendChild(list);
+}
+function card2() {
+  const card1Price = document.getElementById("card-2").innerText;
+  const totalPriceElement = document.getElementById("total-price");
+  const totalPrice = parseFloat(totalPriceElement.innerText);
+  const total = parseFloat(card1Price) + totalPrice;
+  totalPriceElement.innerText = total;
+  document.getElementById("final").innerText = total;
+
+  document.getElementById("final").innerText = total;
+  const cardTitle = document.getElementById("title-2").innerText;
+  const list = document.createElement("li");
+  list.innerHTML = cardTitle;
+  document.getElementById("list").appendChild(list);
+}
+function card3() {
+  const card1Price = document.getElementById("card-3").innerText;
+  const totalPriceElement = document.getElementById("total-price");
+  const totalPrice = parseFloat(totalPriceElement.innerText);
+  const total = parseFloat(card1Price) + totalPrice;
+  totalPriceElement.innerText = total;
+  document.getElementById("final").innerText = total;
+  const cardTitle = document.getElementById("title-3").innerText;
+  const list = document.createElement("li");
+  list.innerHTML = cardTitle;
+  document.getElementById("list").appendChild(list);
+}
+function card4() {
+  const card1Price = document.getElementById("card-4").innerText;
+  const totalPriceElement = document.getElementById("total-price");
+  const totalPrice = parseFloat(totalPriceElement.innerText);
+  const total = parseFloat(card1Price) + totalPrice;
+  totalPriceElement.innerText = total;
+  document.getElementById("final").innerText = total;
+  const cardTitle = document.getElementById("title-4").innerText;
+  const list = document.createElement("li");
+  list.innerHTML = cardTitle;
+  document.getElementById("list").appendChild(list);
+}
+function card5() {
+  const card1Price = document.getElementById("card-5").innerText;
+  const totalPriceElement = document.getElementById("total-price");
+  const totalPrice = parseFloat(totalPriceElement.innerText);
+  const total = parseFloat(card1Price) + totalPrice;
+  totalPriceElement.innerText = total;
+  document.getElementById("final").innerText = total;
+  const cardTitle = document.getElementById("title-5").innerText;
+  const list = document.createElement("li");
+  list.innerHTML = cardTitle;
+  document.getElementById("list").appendChild(list);
+}
+function card6() {
+  const card1Price = document.getElementById("card-6").innerText;
+  const totalPriceElement = document.getElementById("total-price");
+  const totalPrice = parseFloat(totalPriceElement.innerText);
+  const total = parseFloat(card1Price) + totalPrice;
+  totalPriceElement.innerText = total;
+  document.getElementById("final").innerText = total;
+  const cardTitle = document.getElementById("title-6").innerText;
+  const list = document.createElement("li");
+  list.innerHTML = cardTitle;
+  document.getElementById("list").appendChild(list);
+}
